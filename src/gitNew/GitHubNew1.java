a5e7205d9f63a7c33bc0a3612144d500b9a7e5fd
ahmadshahshadan <shadan300@gmail.com>
2023-01-30 22:49:19 -0600

    GitHubnew Project

---
+++ src/gitNew/GitHubNew1.java
@@ -0,0 +1,34 @@
+package gitNew;
+
+public class GitHubNew1 {
+	
+	public static void main(String[] args) {
+		
+		
+		 
+	// 3,6,9,..50
+   
+//		int a = 0 ;  
+//		while (a <25) {
+//	
+//		System.out.println(a);
+//		
+//	       a = a+1;  
+//	     
+//		}      
+//		 
+//		int a = 3;   
+//		do {
+//			System.out.println(a);
+//			a=a+3;
+//		} 
+//		while (a<50);
+//		
+		for (int a = 0; a <3; a++) {
+			for (int b = 0; b<= 3; b++) {
+			
+		System.out.println(a);
+			}
+		}
+	}
+}
